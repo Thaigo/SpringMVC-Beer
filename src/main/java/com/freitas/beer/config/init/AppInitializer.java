4dc2cc364618851cf9a5b78a503b6f5bc8a4fb11
Thaigo_Freitas <tmfreitas@outlook.com>
2018-02-03 03:19:41 -0200

    setup inicial

---
+++ src/main/java/com/freitas/beer/config/init/AppInitializer.java
@@ -0,0 +1,25 @@
+package com.freitas.beer.config.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.freitas.beer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
---
+++ src/main/java/com/freitas/beer/controller/CervejasController.java
@@ -0,0 +1,8 @@
+package com.freitas.beer.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CervejasController {
+
+}
