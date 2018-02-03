e44c636b9354dea64379668fbfe6eb541703f321
Thaigo_Freitas <tmfreitas@outlook.com>
2018-02-03 20:25:03 -0200

    3.9 v2 Desafio Validando a descricao

--- src/main/java/com/freitas/beer/controller/CervejasController.java
+++ src/main/java/com/freitas/beer/controller/CervejasController.java
@@ -22,7 +22,8 @@ public class CervejasController {
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST) 
 	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		
-		if (result.hasErrors()) {			
+		if (result.hasErrors()) {		
+			model.addAttribute("mensagem", "Erro no Fomalulario");
 			System.out.println("Houve um erro, Todos o campos devem ser preenchidos");
 			return "cerveja/CadastroCerveja";
 		}
