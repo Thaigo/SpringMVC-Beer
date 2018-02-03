072236f64fec79690faffa31e16c91b89152178f
Thaigo_Freitas <tmfreitas@outlook.com>
2018-02-03 18:04:15 -0200

    3.6 comecando o cadastro de cerveja

--- src/main/java/com/freitas/beer/controller/CervejasController.java
+++ src/main/java/com/freitas/beer/controller/CervejasController.java
@@ -2,12 +2,22 @@ package com.freitas.beer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.freitas.beer.model.Cerveja;
 
 @Controller
 public class CervejasController {
 
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST) 
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println("cadastrado Sku: " + cerveja.getSku() + " Realizado com sucesso");
+		System.out.println("cadastrado Nome: " + cerveja.getNome() + " Realizado com sucesso");
 		return "cerveja/CadastroCerveja";
 	}
 	
