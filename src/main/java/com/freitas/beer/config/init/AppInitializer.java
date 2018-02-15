fa2f00cc7e24ab8971952e6d45917412b3d1f98d
Thaigo_Freitas <tmfreitas@outlook.com>
2018-02-15 16:22:58 -0200

    14.3 Upload da foto com Ajax

--- src/main/java/com/freitas/beer/config/init/AppInitializer.java
+++ src/main/java/com/freitas/beer/config/init/AppInitializer.java
@@ -1,6 +1,9 @@
 package com.freitas.beer.config.init;
 
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.freitas.beer.config.ServiceConfig;
@@ -22,6 +25,12 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}	
+	
+	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(new MultipartConfigElement(""));
+	}
 	
 
 }
